3c33fe1fc261c0853f81a9fc86a630fda3d1eab3
qchenevier <qchenevier@users.noreply.github.com>
2018-05-01 14:10:11 +0200

    added markdown component

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,21 +1,21 @@
 <template>
   <article>
     <h1>{{ title }}</h1>
-    {{ body }}
+    <vue-markdown> {{ body }} </vue-markdown>
   </article>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown'
+
 export default {
   async asyncData({ params }) {
-    // const postPromise = process.BROWSER_BUILD
-    //   ? import('~/content/blog/posts/' + params.slug + '.json')
-    //   : Promise.resolve(
-    //       require('~/content/blog/posts/' + params.slug + '.json')
-    //     );
-
     let post = await import('~/content/blog/posts/' + params.slug + '.json');
     return post;
+},
+  components: {
+      VueMarkdown
   }
 };
+
 </script>
