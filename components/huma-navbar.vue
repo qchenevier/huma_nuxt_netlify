7e29a82970724fddcf10db319a4809060244c840
qchenevier <qchenevier@users.noreply.github.com>
2018-05-10 22:33:16 +0200

    created huma-navbar component

---
+++ components/huma-navbar.vue
@@ -0,0 +1,65 @@
+<template>
+  <navbar class="navbar container is-fixed-top">
+
+    <div class="navbar-brand">
+
+      <a href="http://huma.io">
+        <figure class="image is-96x96 huma_logo" href="http://huma.io">
+          <img src="~/assets/huma_logo.svg"/>
+        </figure>
+      </a>
+
+      <div
+        class="navbar-burger"
+        @click="showNav = !showNav"
+        :class="{'is-active': showNav}"
+        style="height: 104px;"
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+    </div>
+
+    <div
+      class="navbar-menu"
+      :class="{'is-active': showNav}"
+    >
+
+      <div class="navbar-start">
+        <a class="navbar-item" href="http://huma.io"> Home </a>
+        <a class="navbar-item" href="http://huma.io"> Blog </a>
+      </div>
+
+    </div>
+
+  </navbar>
+</template>
+
+
+<script>
+export default {
+  data () {
+    return { showNav: false }
+  },
+  head: {
+      htmlAttrs: {
+        class: "has-navbar-fixed-top"
+      }
+  }
+};
+</script>
+
+<style>
+html.has-navbar-fixed-top {
+    padding-top: 113px;
+}
+
+.huma_logo {
+  margin-top: 8px;
+  margin-right: 32px;
+  margin-bottom: 8px;
+  margin-left: 8px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,50 +1,21 @@
 <template>
   <div>
-    <nav class="navbar container is-fixed-top">
 
-      <div class="navbar-brand">
+    <huma-navbar/>
 
-        <a href="http://huma.io">
-          <figure class="image is-96x96 huma_logo" href="http://huma.io">
-            <img src="~/assets/huma_logo.svg"/>
-          </figure>
-        </a>
-
-        <div class="navbar-burger" @click="showNav = !showNav" :class="{ 'is-active': showNav }"  style="height: 104px;">
-          <span></span>
-          <span></span>
-          <span></span>
+<!--
+      <section class="hero is-medium">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              HUMA
+            </h1>
+            <h2 class="subtitle">
+              A blog about Humans & Machines
+            </h2>
+          </div>
         </div>
-
-      </div>
-
-      <div class="navbar-menu" :class="{ 'is-active': showNav }">
-
-        <div class="navbar-start">
-          <a class="navbar-item" href="http://huma.io">
-            Home
-          </a>
-          <a class="navbar-item" href="http://huma.io">
-            Blog
-          </a>
-        </div>
-
-      </div>
-
-    </nav>
-
-    <section class="hero">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
-            HUMA
-          </h1>
-          <h2 class="subtitle">
-            A blog about Humans & Machines
-          </h2>
-        </div>
-      </div>
-    </section>
+      </section> -->
 
     <div class="container">
       <nuxt/>
@@ -54,18 +25,11 @@
 </template>
 
 <script>
+import HumaNavbar from '~/components/huma-navbar.vue'
+
 export default {
-  data() {
-    return { showNav: false }
+  components: {
+    HumaNavbar,
   }
 };
 </script>
-
-<style>
-.huma_logo {
-  margin-top: 8px;
-  margin-right: 32px;
-  margin-bottom: 8px;
-  margin-left: 8px;
-}
-</style>
