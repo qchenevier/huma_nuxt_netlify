46db299c2a5567df1af7855e4e0763a732a895c1
qchenevier <qchenevier@users.noreply.github.com>
2018-05-11 20:09:22 +0200

    change title & favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'huma_nuxt_netlify',
+    title: 'HUMA - Humans & Machines',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
