cae496283d04ee9f9a69c2e4bbba486970075785
qchenevier <qchenevier@users.noreply.github.com>
2018-06-03 15:44:52 +0200

    added tags

--- components/huma-post-card.vue
+++ components/huma-post-card.vue
@@ -4,6 +4,9 @@
       .card-image
          img(:src="post.thumbnail")
       .card-content
+        .content(v-if="post.tags")
+          b-taglist(v-for="tag in post.tags" :key="tag.name")
+            b-tag tag.name
         .content
           p.title {{ post.title }}
           p.subtitle {{ post.summary }}
