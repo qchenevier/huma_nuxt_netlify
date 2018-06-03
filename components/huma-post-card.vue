0e4547bdad6b3b0b9448957a85f8778eabeda250
qchenevier <qchenevier@users.noreply.github.com>
2018-06-03 18:20:21 +0200

    corrected tags in post card

--- components/huma-post-card.vue
+++ components/huma-post-card.vue
@@ -5,8 +5,10 @@
          img(:src="post.thumbnail")
       .card-content
         .content(v-if="post.tags")
-          b-taglist(v-for="tag in post.tags" :key="tag.name")
-            b-tag {{ tag.name }}
+          b-taglist(grouped, group-multiline)
+            b-tag(v-for="tag in post.tags", :key="tag.tag")
+              b-icon(size="is-small", :icon="tag.icon")
+              | &nbsp {{ tag.tag }}
         .content
           p.title {{ post.title }}
           p.subtitle {{ post.summary }}
