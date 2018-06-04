d1d1e0ab8df512f72d8d8378eba279a96ab4e875
qchenevier <qchenevier@users.noreply.github.com>
2018-06-04 08:22:42 +0200

    corrected issue with repeating tags

--- components/huma-post-card.vue
+++ components/huma-post-card.vue
@@ -1,21 +1,20 @@
 <template lang='pug'>
-  .card
-    nuxt-link.has-text-dark(:to="post._path")
-      .card-image
-         img(:src="post.thumbnail")
-      .card-content
-        .content(v-if="post.tags")
-          b-taglist
-            nuxt-link(tag="b-tag", v-for="tag in post.tags", :key="tag.tag", :to="tag_href(tag)", @click.stop="handleArrow")
-              a.has-text-dark
-                b-icon(size="is-small", :icon="tag.icon")
-                | &nbsp {{ tag.tag }}
-        .content
-          p.title {{ post.title }}
-          p.subtitle {{ post.summary }}
-      .card-footer
-        .card-footer-item {{ reading_time.text }}
-        .card-footer-item {{ pretty_date }}
+  nuxt-link.has-text-dark(class="card", tag="div", :to="post._path")
+    .card-image
+       img(:src="post.thumbnail")
+    .card-content
+      .content(v-if="post.tags")
+        b-taglist
+          nuxt-link(tag="b-tag", v-for="tag in post.tags", :key="tag.tag", :to="tag_href(tag)", @click.stop="handleArrow")
+            a.has-text-dark
+              b-icon(size="is-small", :icon="tag.icon")
+              | &nbsp {{ tag.tag }}
+      .content
+        p.title {{ post.title }}
+        p.subtitle {{ post.summary }}
+    .card-footer
+      .card-footer-item {{ reading_time.text }}
+      .card-footer-item {{ pretty_date }}
 </template>
 
 <script>
